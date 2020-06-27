4c9f56f5fa645f1ff5b57750fd905d43d23d215e
hirokidaichi <hirokidaichi@gmail.com>
2020-06-27 11:52:52 +0900

    gilot author added

--- src/gilot/__init__.py
+++ src/gilot/__init__.py
@@ -3,4 +3,4 @@
 from .core import from_csv, from_csvs, from_dir  # NOQA
 from .hotgraph import hotgraph  # NOQA
 from .hotspot import hotspot  # NOQA
-from .plot import info, plot  # NOQA
+from .plot import info, plot ,authors  # NOQA
